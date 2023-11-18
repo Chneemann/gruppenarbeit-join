022789ece1ab8559cb147c14a6b0b1be1109b8a9
Chneemann <Andre-Ke@gmx.de>
2023-11-18 11:24:03 +0100

    add return to login-page button

--- js/login_signup.js
+++ js/login_signup.js
@@ -2,3 +2,8 @@ function loadSignupPage() {
   document.getElementById("signup-page").classList.remove("d-none");
   document.getElementById("login-page").classList.add("d-none");
 }
+
+function loadLoginPage() {
+  document.getElementById("signup-page").classList.add("d-none");
+  document.getElementById("login-page").classList.remove("d-none");
+}
