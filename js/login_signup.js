df8993962952635ec7157e83af16877fb21ccddd
Chneemann <Andre-Ke@gmx.de>
2023-11-18 14:47:18 +0100

    add loading logo

--- js/login_signup.js
+++ js/login_signup.js
@@ -1,3 +1,13 @@
+function moveLogoToTopLeft() {
+  setTimeout(() => {
+    document.querySelector(".logo-banner").classList.add("move-to-top-left");
+  }, "2000");
+  setTimeout(() => {
+    loadLoginPage();
+    document.getElementById("logo-banner").classList.add("d-none");
+  }, "4000");
+}
+
 function loadSignupPage() {
   document.getElementById("signup-page").classList.remove("d-none");
   document.getElementById("login-page").classList.add("d-none");
