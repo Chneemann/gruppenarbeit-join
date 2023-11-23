90d1426de3aea0cf6ad88267fd523eb76fdc72b2
Chneemann <Andre-Ke@gmx.de>
2023-11-23 10:37:42 +0100

    merge task overlay & board.html

---
+++ js/board.js
@@ -0,0 +1,7 @@
+function openCart() {
+  document.getElementById("task-overlay-cart").classList.remove("d-none");
+}
+
+function closeCart() {
+  document.getElementById("task-overlay-cart").classList.add("d-none");
+}
