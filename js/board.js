c812a384ac1b9f7f72d901b5ae93499ceec9c539
Chneemann <Andre-Ke@gmx.de>
2023-11-23 11:06:30 +0100

    add dialog-cart fade in

--- js/board.js
+++ js/board.js
@@ -1,7 +1,9 @@
 function openCart() {
-  document.getElementById("task-overlay-cart").classList.remove("d-none");
+  document.getElementById("task-overlay-cart").classList.add("dialog-show");
+  document.getElementById("task-overlay-cart").classList.remove("dialog-hide");
 }
 
 function closeCart() {
-  document.getElementById("task-overlay-cart").classList.add("d-none");
+  document.getElementById("task-overlay-cart").classList.add("dialog-hide");
+  document.getElementById("task-overlay-cart").classList.remove("dialog-show");
 }
