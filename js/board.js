0f6b623fa4a37d91068bb598fd450b226c5fb9e7
Chneemann <Andre-Ke@gmx.de>
2023-11-24 09:24:45 +0100

    add dialog add task

--- js/board.js
+++ js/board.js
@@ -1,3 +1,25 @@
+// TASK
+
+async function openAddTask() {
+  var overlay = document.getElementById("add-task-dialog");
+  overlay.classList.remove("d-none");
+  await sleep(10);
+  overlay.classList.add("dialog-show");
+  overlay.classList.remove("dialog-hide");
+  changeBackground(overlay);
+}
+
+async function closeAddTask() {
+  var overlay = document.getElementById("add-task-dialog");
+  overlay.classList.add("dialog-hide");
+  overlay.classList.remove("dialog-show");
+  overlay.style.backgroundColor = "";
+  await sleep(100);
+  overlay.classList.add("d-none");
+}
+
+// CART
+
 async function openCart() {
   var overlay = document.getElementById("task-overlay-cart");
   overlay.classList.remove("d-none");
@@ -15,6 +37,8 @@ async function closeCart() {
   await sleep(100);
   overlay.classList.add("d-none");
 }
+
+// GLOBAL
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
