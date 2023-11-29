9a2fe16e32764b5af827794ed152c019114ac781
Chneemann <Andre-Ke@gmx.de>
2023-11-29 12:23:41 +0100

    add mobile navbar

--- script.js
+++ script.js
@@ -32,15 +32,19 @@ function includeNavbarHTML() {
     if (file) {
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("w3-include-html");
           includeNavbarHTML();
         }
-      }
+      };
       xhttp.open("GET", file, true);
       xhttp.send();
       /* Exit the function: */
@@ -49,3 +53,18 @@ function includeNavbarHTML() {
   }
 }
 
+window.addEventListener("DOMContentLoaded", (event) => {
+  function handleResize() {
+    if (window.innerWidth < 860) {
+      document.getElementById("mobile-header").classList.remove("d-none");
+      document.getElementById("navbar").classList.add("d-none");
+      document.getElementById("mobile-navbar").classList.remove("d-none");
+    } else {
+      document.getElementById("mobile-header").classList.add("d-none");
+      document.getElementById("navbar").classList.remove("d-none");
+      document.getElementById("mobile-navbar").classList.add("d-none");
+    }
+  }
+  handleResize();
+  window.addEventListener("resize", handleResize);
+});
