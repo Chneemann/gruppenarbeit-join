01c336d1b5b9273190d896d5b6c74dd4f7a5df50
DaLauri <s2020@posteo.de>
2023-12-03 11:52:40 +0100

    Funktionen Kontakte

--- js/address.js
+++ js/address.js
@@ -18,18 +18,54 @@ let contacts = [
         'color': '#218766',
     }
 ]
-function addContact() {
-    document.getElementById("add-contact-overlay").style.display = "flex";
+
+function openAddContact(){
+ document.getElementById("add-contact-overlay").style.display = "flex";
     document.getElementById("overlay-header").innerHTML='Add Contact';
 
 }
+function addContact() {
+   
+    let newName = document.getElementById('name').value;
+    let newEmail = document.getElementById("email").value;
+    let newPhone = document.getElementById("phone").value;
+    let newColor = randomColor = Math.floor(Math.random()*16777215).toString(16);
+    contacts.push({newName,newEmail,newPhone,newColor});
+    console.log(contacts[contacts.length-1]);
+    console.log(contacts);
 
-function editContact(){
+    renderContacts();
+}
+
+function editContact(i){
     document.getElementById("add-contact-overlay").style.display = "flex";
     document.getElementById("overlay-header").innerHTML='Edit Contact';
+    document.getElementById("submit-contact").innerHTML='Save';
+    document.getElementById("delete-close-contact").innerHTML=`
+    <span onclick="deleteContact()" >cancel</span>
+    `;
+    document.getElementById("name").value = contacts[i]['name'];
+    document.getElementById("email").value = contacts[i]['email'];
+    document.getElementById("phone").value = contacts[i]['phone'];
+    document.getElementById("overlay-badge").classList.add('big-badge');
+    document.getElementById("overlay-badge").style.backgroundColor=contacts[i]['color'];
+    document.getElementById("overlay-badge").innerHTML=`
+    <span>${getInitials(i)}</span>
+    `;
+}
+
+function deleteContact(i){
+    console.log('Delete');
 }
 function closeAddContact() {
     document.getElementById("add-contact-overlay").style.display = "none";
+    resetForm();
+}
+
+function resetForm(){
+    document.getElementById("name").value = '';
+    document.getElementById("email").value ='';
+    document.getElementById("phone").value ='';
 }
 
 function firstLetter(i) { //FUNKTION FÜR REGISTER
@@ -54,10 +90,6 @@ function getInitials(i) {// FUNKTION FÜR BADGES
     return initials.join("") //Methode entfernt das Komma
 }
 
-function test(path){
-    console.log('test');
-
-}
 
 function renderContacts() {
     console.log('render contacts');
@@ -86,8 +118,8 @@ function viewCard(i) {
             </div>
             <div class='card-closeup-header-right'><h2>${contacts[i]['name']}</h2> 
                 <div class='edit-delete-contact'>
-                    <div class='edit-contact' onclick='editContact()'><img src="./assets/img/edit.svg">Edit</div>
-                    <div class='delete-contact' onclick='deleteContact()'><img src="./assets/img/delete.svg">Delete</div>
+                    <div class='edit-contact' onclick='editContact(${i})'><img src="./assets/img/edit.svg">Edit</div>
+                    <div class='delete-contact' onclick='deleteContact(${i})'><img src="./assets/img/delete.svg">Delete</div>
                 </div>
             </div>
         </div>
