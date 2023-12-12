2759f35b4d4d23a036d5b9c46cbc4625c9114569
Chneemann <Andre-Ke@gmx.de>
2023-12-12 12:16:31 +0100

    bugfixes

--- js/address.js
+++ js/address.js
@@ -12,13 +12,13 @@ function renderContacts() {
     let contact = contacts[i];
 
     document.getElementById("contact-list").innerHTML += /*html*/ `
-        <div class="single-contact" id='contact-${i}' onclick='viewCard(${i})'>
-        <div class='badge' style="background-color:${contact["color"]}" ><span>${contact["initials"]}</span></div>
-        <div class='card'>
-           <span>${contact["name"]}</span>
-            <p>${contact["email"]}</p>
+      <div class="single-contact" id='contact-${i}' onclick='viewCard(${i})'>
+          <div class='badge' style="background-color:${contact["color"]}" ><span>${contact["initials"]}</span></div>
+          <div class='card'>
+            <span>${contact["name"]}</span>
+              <p>${contact["email"]}</p>
+      </div>
     </div>
-</div>
        `;
   }
 }
@@ -47,15 +47,22 @@ async function openAddContact() {
   renderContactOverlay();
   await openDialog("add-contact");
   document.getElementById("overlay-header").innerHTML = "Add Contact";
-  document.getElementById(
-    "submit-buttons"
-  ).innerHTML = `<button class="add-contact-btn-clear" id="delete-close-contact" onclick="closeAddContact()"><span>cancel</span><img
-    src="./assets/img/close.svg"/>
-    </button>
-
-    <button type="submit" class="add-contact-btn-create" id="submit-contact" onclick="addContact()"><span>Create
-        contact</span><img src="./assets/img/check.svg" />
-    </button>
+  document.getElementById("form-input").innerHTML = /*html*/ `
+  <form class="contact-inputs" onsubmit="addContact(); return false">
+  <input required type="text" placeholder='Name' id="name" autocomplete="none">
+  <input required type="email" placeholder='Email' id="email" autocomplete="none">
+  <input type="tel" placeholder='Phone' id="phone" autocomplete="none"> 
+    <div id="submit-buttons">
+      <button class="add-contact-btn-clear" id="delete-close-contact" onclick="closeAddContact()">
+      <span>cancel</span>
+      <img src="./assets/img/close.svg"/>
+      </button>
+      <button type="submit" class="add-contact-btn-create" id="submit-contact" onclick="">
+      <span>Create contact</span>
+      <img src="./assets/img/check.svg" />
+      </button>
+    </div>
+    </form>
     `;
 }
 
@@ -75,49 +82,32 @@ function closeAddContact() {
 async function addContact() {
   let name = document.getElementById("name").value;
   name = capitalizeFirstLetter(name);
-  if (name) {
+  if (checkNameValidity(name) && name) {
     let email = document.getElementById("email").value;
     let phone = document.getElementById("phone").value;
     let initials = getInitials(name);
     let color = getRandomColor();
     let id = contacts.length;
-
     contacts.push({ id, name, initials, email, phone, color });
-
     setItem("contacts", contacts);
     closeAddContact();
-    await createContactAlert();
-
     resetPage();
     renderContacts();
+    await sleep(300);
+    openInformationWindow("The contact was successfully created", 2500);
   }
 }
 
-/**
- * Makes a slider message appear, which notifies that a new contact has been created
- * detects if screen width to set the direction of the slider
- */
-async function createContactAlert() {
-  document.getElementById("contact-alert").classList.remove("d-none");
-  await sleep(1000);
-  let mobileMode = window.innerWidth < 860;
-
-  document.getElementById("contact-alert").classList.add("transition");
-
-  if (mobileMode) {
-    document.getElementById("contact-alert").style.transform = "translateY(0)";
-    await sleep(2000);
-    document.getElementById("contact-alert").style.transform =
-      "translateY(100vh)";
-    await sleep(1000);
+function checkNameValidity(name) {
+  const fullNameValue = name.trim();
+  const nameRegex = /^(\S+\s+\S+)$/;
+  const isValidName = nameRegex.test(fullNameValue);
+  if (!isValidName) {
+    openInformationWindow("You must enter a first name and a surname.", 3000);
+    return false;
   } else {
-    document.getElementById("contact-alert").style.transform = "translateX(0)";
-    await sleep(2000);
-    document.getElementById("contact-alert").style.transform =
-      "translateX(100vw)";
-    await sleep(1000);
+    return true;
   }
-  document.getElementById("contact-alert").classList.add("d-none");
 }
 
 /**
@@ -126,27 +116,19 @@ async function createContactAlert() {
 function renderContactOverlay(id) {
   document.getElementById("add-contact-overlay").innerHTML = /*html*/ `
     <div id="add-contact" class="dialog-hide" onclick="event.stopPropagation()">
-            <div class="add-contact-left"><img src="./assets/img/Join logo light.png" alt="">
-                <h1 id="overlay-header">Add contact</h1>
-                <span id="subtitle">Tasks are better with a Team </span>
-                <img src="./assets/img/Vector 5.png" alt="">
-            </div>
-            <div id="overlay-badge">
-                <img class="big-badge"  src="./assets/img/Group 13.png" alt="">
-            </div>
-            <div class="add-contact-form">
-                <img src="./assets/img/close.svg" class="overlay-close-x" onclick="closeAddContact()">
-
-                <form class="contact-inputs" onsubmit="editContact(${id}); return false">
-                    <input required type="text" placeholder='Name' id="name" autocomplete="none">
-                    <input required type="email" placeholder='Email' id="email" autocomplete="none">
-                    <input type="tel" placeholder='Phone' id="phone" autocomplete="none">
-                    <div id="submit-buttons"></div>
-</form>
-                
-            </div>
+        <div class="add-contact-left"><img src="./assets/img/Join logo light.png" alt="">
+            <h1 id="overlay-header">Add contact</h1>
+            <span id="subtitle">Tasks are better with a Team </span>
+            <img src="./assets/img/Vector 5.png" alt="">
         </div>
-    
+        <div id="overlay-badge">
+            <img class="big-badge"  src="./assets/img/Group 13.png" alt="">
+        </div>
+        <div class="add-contact-form">
+            <img src="./assets/img/close.svg" class="overlay-close-x" onclick="closeAddContact()">
+            <div id="form-input"></div>             
+        </div>
+    </div>
     `;
 }
 
@@ -158,20 +140,22 @@ async function openEditContact(id) {
   resetPage();
   renderContactOverlay(id);
   await openDialog("add-contact");
-
   document.getElementById("overlay-header").innerHTML = "Edit Contact";
   document.getElementById("subtitle").classList.add("none");
-  document.getElementById(
-    "submit-buttons"
-  ).innerHTML = ` <button class="add-contact-btn-clear" id="delete-close-contact" onclick="deleteContact(${id}), closeAddContact()"><span>Delete</span>
-</button>
-
-<button type="submit" class="add-contact-btn-create" id="submit-contact"><span>Save</span><img src="./assets/img/check.svg" />
-</button>
+  document.getElementById("form-input").innerHTML = /*html*/ `
+  <form class="contact-inputs" onsubmit="editContact(${id}); return false">
+                <input required type="text" placeholder='Name' id="name" autocomplete="none">
+                <input required type="email" placeholder='Email' id="email" autocomplete="none">
+                <input type="tel" placeholder='Phone' id="phone" autocomplete="none">
+                <div id="submit-buttons">
+    <button class="add-contact-btn-clear" id="delete-close-contact" onclick="deleteContact(${id}), closeAddContact()"><span>Delete</span>
+    </button>
+    <button type="submit" class="add-contact-btn-create" id="submit-contact"><span>Save</span><img src="./assets/img/check.svg" />
+    </button>
+</div>
+</form>
     `;
-
   fillInput(id);
-
   document.getElementById("delete-close-contact").innerHTML = `
     <span onclick="deleteContact(${id})" >Delete</span>
     `;
@@ -201,13 +185,15 @@ async function editContact(id) {
   let phone = document.getElementById("phone").value;
   let initials = getInitials(name);
   let color = contacts[id]["color"];
-  contacts.splice(id, 1, { id, name, initials, email, phone, color });
-  await setItem("contacts", JSON.stringify(contacts));
-  resetPage();
-  closeAddContact();
-  renderContacts();
-  await sleep(300);
-  openInformationWindow("The contact information has been saved", 2500);
+  if (checkNameValidity(name)) {
+    contacts.splice(id, 1, { id, name, initials, email, phone, color });
+    await setItem("contacts", JSON.stringify(contacts));
+    resetPage();
+    closeAddContact();
+    renderContacts();
+    await sleep(300);
+    openInformationWindow("The contact information has been saved", 2500);
+  }
 }
 
 /**
@@ -271,7 +257,8 @@ function openContactOptions(i) {
   document.getElementById("contact-options").innerHTML += `
     <div class='contact-options'>
     <div class='edit-contact' onclick='openEditContact(${i})'><img src="./assets/img/edit.svg">Edit</div>
-    <div class='delete-contact' onclick='deleteContact(${i})'><img src="./assets/img/delete.svg">Delete</div></div>
+    <div class='delete-contact' onclick='deleteContact(${i})'><img src="./assets/img/delete.svg">Delete</div>
+    </div>
         `;
 }
 
@@ -340,34 +327,29 @@ function firstLetter(i) {
  * Shows the selected contact with all information next to the contact list(desktop mode)
  */
 function renderCardCloseup(i) {
-  document.getElementById("card-closeup").innerHTML =
-    /*html*/
-    `
-       
-        <div class='card-closeup-header'>
-            <div class='big-badge' style='background-color:${contacts[i]["color"]}'>
-            <span>${contacts[i]["initials"]}</span>
-            </div>
-            <div class='card-closeup-header-right'><h2>${contacts[i]["name"]}</h2> 
-                <div class='edit-delete-contact'>
-                    <div class='edit-contact' onclick='openEditContact(${i})'><img src="./assets/img/edit.svg">Edit</div>
-                    <div class='delete-contact' onclick='deleteContact(${i})'><img src="./assets/img/delete.svg">Delete</div>
-                </div>
-            </div>
-        </div>
-
+  document.getElementById("card-closeup").innerHTML = /*html*/ `
+    <div class='card-closeup-header'>
+      <div class='big-badge' style='background-color:${contacts[i]["color"]}'>
+      <span>${contacts[i]["initials"]}</span>
+      </div>
+      <div class='card-closeup-header-right'><h2>${contacts[i]["name"]}</h2> 
+          <div class='edit-delete-contact'>
+              <div class='edit-contact' onclick='openEditContact(${i})'><img src="./assets/img/edit.svg">Edit</div>
+              <div class='delete-contact' onclick='deleteContact(${i})'><img src="./assets/img/delete.svg">Delete</div>
+          </div>
+      </div>
+    </div>
     <div class='contact-information'>
-           
-           <h3> Contact Information</h3>
-           <p>Email</p>
-            ${contactEmail}
-            <br>
-            <p>Phone</p>
-            <span>${contactPhone}</span>
+      <h3> Contact Information</h3>
+      <p>Email</p>
+        ${contactEmail}
+        <br>
+        <p>Phone</p>
+        <span>${contactPhone}</span>
     </div>
     <button onclick="openContactOptions(${i})" class="button-open-add-contact-mobile2" id="contact-options">
-                <img src="./assets/img/Menu Contact Options.png">
-            </button>
+    <img src="./assets/img/Menu Contact Options.png">
+    </button>
     `;
 }
 
